63cbafc1910c0314aeba40dc805ca6d4401697a6
Tomáš <hackergamingcz@gmail.com>
2018-05-30 20:05:48 +0200

    spectator listener

--- src/main/java/cz/HackerGamingCZ/HackerTools/listeners/Spectator.java
+++ src/main/java/cz/HackerGamingCZ/HackerTools/listeners/Spectator.java
@@ -1,6 +1,45 @@
 package cz.HackerGamingCZ.HackerTools.listeners;
 
+import cz.HackerGamingCZ.HackerTools.HackerTools;
+import cz.HackerGamingCZ.HackerTools.players.HTPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class Spectator implements Listener {
+
+    private boolean isSpectator(Player player) {
+        HTPlayer htPlayer = HackerTools.getPlugin().getPlayerManager().getPlayer(player);
+        return htPlayer.isSpectator();
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent e) {
+        if (!(e.getDamager() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) e.getDamager();
+        if (isSpectator(player)) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(BlockBreakEvent e) {
+        if (isSpectator(e.getPlayer())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(BlockPlaceEvent e) {
+        if (isSpectator(e.getPlayer())) {
+            e.setCancelled(true);
+        }
+    }
+
+
 }
