a916ebcca78e344ee85fbec9846d4cc59eb3f0cd
Tomáš <hackergamingcz@gmail.com>
2018-06-02 18:54:30 +0200

    Kickall

--- src/main/java/cz/HackerGamingCZ/HackerTools/managers/ServerManager.java
+++ src/main/java/cz/HackerGamingCZ/HackerTools/managers/ServerManager.java
@@ -1,11 +1,26 @@
 package cz.HackerGamingCZ.HackerTools.managers;
 
+import cz.HackerGamingCZ.HackerTools.Permissions;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class ServerManager {
 
     public void restart(){
         Bukkit.getServer().shutdown();
+    }
+
+    public void kickAll(boolean usePermission, String reason) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (usePermission && Permissions.hasPermission(p, Permissions.KICKALL_PROTECTION)) {
+                return;
+            }
+            if (reason != null && reason.length() > 0) {
+                p.kickPlayer("§6You have been kicked by system. Reason: §e" + reason);
+            } else {
+                p.kickPlayer("§6You have been kicked by system.");
+            }
+        }
     }
 
 }
