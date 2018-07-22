9500a07ac2ac40487e13fa27b70913d078208fda
Tomáš Střecha <thomascze7@outlook.cz>
2018-07-22 12:31:35 +0200

    Added enum LeatherArmorMaterial for better function of LeatherArmorBuilder

--- src/main/java/cz/HackerGamingCZ/HackerTools/builder/LeatherArmorBuilder.java
+++ src/main/java/cz/HackerGamingCZ/HackerTools/builder/LeatherArmorBuilder.java
@@ -15,22 +15,22 @@ public class LeatherArmorBuilder extends ItemBuilder {
 
     private LeatherArmorMeta armorMeta = (LeatherArmorMeta) super.getItemMeta();
 
-    public LeatherArmorBuilder(Material material) {
+    public LeatherArmorBuilder(LeatherArmorMaterial material) {
         this(material, 1, (byte) 0);
     }
 
-    public LeatherArmorBuilder(Material material, int amount) {
+    public LeatherArmorBuilder(LeatherArmorMaterial material, int amount) {
         this(material, amount, (byte) 0);
     }
 
-    public LeatherArmorBuilder(Material material, int amount, byte data) {
-        super(material, amount, data);
+    public LeatherArmorBuilder(LeatherArmorMaterial material, int amount, byte data) {
+        super(material.getBukkitMaterial(), amount, data);
         Material[] allowedMaterials1 = {Material.LEATHER_CHESTPLATE, Material.LEATHER_BOOTS, Material.LEATHER_LEGGINGS, Material.LEATHER_HELMET};
         List allowedMaterials = Arrays.asList(allowedMaterials1);
 
     }
 
-    public LeatherArmorBuilder(Material material, byte data) {
+    public LeatherArmorBuilder(LeatherArmorMaterial material, byte data) {
         this(material, 1, data);
     }
 
@@ -42,5 +42,23 @@ public class LeatherArmorBuilder extends ItemBuilder {
     @Override
     public LeatherArmorMeta getItemMeta() {
         return armorMeta;
+    }
+
+    public enum LeatherArmorMaterial {
+
+        HELMET(Material.LEATHER_HELMET),
+        CHESTPLATE(Material.LEATHER_CHESTPLATE),
+        LEGGINGS(Material.LEATHER_LEGGINGS),
+        BOOTS(Material.LEATHER_BOOTS);
+
+        private Material bukkitMaterial;
+
+        LeatherArmorMaterial(Material bukkitMaterial) {
+            this.bukkitMaterial = bukkitMaterial;
+        }
+
+        public Material getBukkitMaterial() {
+            return bukkitMaterial;
+        }
     }
 }
